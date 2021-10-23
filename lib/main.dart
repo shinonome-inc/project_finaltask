09a901b83c32bfd0f7c20f643fefc139b7b9a78f
MiyuSato <83227352+MiyuSato@users.noreply.github.com>
2021-10-23 14:46:45 +0900

    fix color indent

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -22,112 +21,114 @@ class TopPage extends StatefulWidget {
   _TopPageState createState() => _TopPageState();
 }
 
-class _TopPageState extends State<TopPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(
-            decoration: BoxDecoration(
-             image: DecorationImage(
-              image: AssetImage('assets/images/backgroundimage.png'),
-              fit: BoxFit.cover,
-          ),
-         ),
-        ),
-          Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget>[
-              Expanded(
-                flex: 5,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                            'Qiita Feed App',
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontFamily: 'Pacifico',
-                          color: Color.fromRGBO(255, 255, 255, 1.0),
-                        ),
-                        ),
-                        Text(
-                          '-PlayGround-',
-                        style: TextStyle(
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            letterSpacing: 0.25,
-                            color: Color.fromRGBO(255, 255, 255, 1.0),
-                        ),),
-                      ],
-                    ),
-                  )
-              ),
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.all(10.0),
-                            child: SizedBox(
-                              width: 327,
-                              height: 50,
-                              child: TextButton(
-                                onPressed: (){},
-                                style: TextButton.styleFrom(
-                                  backgroundColor: Color.fromRGBO(70, 131, 0, 1.0),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(25.0),
-                                  ),
-                                ),
-                                child: Text(
-                                  'ログインする',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontStyle: FontStyle.normal,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14,
-                                    letterSpacing: 0.75,
-                                    color: Color.fromRGBO(249, 252, 255, 1.0)
-                                 ),
-                                ),
-                               ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                              child: TextButton(
-                               child: Text(
-                                'ログインせずに利用する',
-                                 textAlign: TextAlign.center,
-                                 style: TextStyle(
-                                   fontStyle: FontStyle.normal,
-                                   fontWeight: FontWeight.bold,
-                                   fontSize: 14,
-                                   letterSpacing: 0.75,
-                                   color: Color.fromRGBO(255, 255, 255, 1.0),
-                                ),
-                              ),
-                              onPressed: (){},
-                            ),
-                            ),
-                           ],
-                            ),
-                             ),
-                             ),
-              ],
-            )
-          ]
-        )
-    );
+extension ColorExtension on String {
+  toColor() {
+    var hexColor = this.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
   }
 }
 
-
-
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/backgroundimage.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              flex: 5,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Qiita Feed App',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontFamily: 'Pacifico',
+                        color: '#ffffff'.toColor(),
+                      ),
+                    ),
+                    Text(
+                      '-PlayGround-',
+                      style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        letterSpacing: 0.25,
+                        color: '#ffffff'.toColor(),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(
+                    width: 327,
+                    height: 50,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: '#468300'.toColor(),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                      child: Text(
+                        'ログインする',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          letterSpacing: 0.75,
+                          color: '#ffffff'.toColor(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    child: Text(
+                      'ログインせずに利用する',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        letterSpacing: 0.75,
+                        color: '#ffffff'.toColor(),
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 64,
+          ),
+        ],
+      )
+    ]));
+  }
+}
