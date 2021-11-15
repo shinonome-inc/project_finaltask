2cb5e19cd24c52af8282e3aeb5382df5423a752b
MiyuSato <83227352+MiyuSato@users.noreply.github.com>
2021-11-15 22:58:52 +0900

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:project_finaltask/bottom.dart';
 
 import 'color_extension.dart';
 
@@ -111,7 +112,10 @@ class _TopPageState extends State<TopPage> {
                         color: '#ffffff'.toColor(),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Bottom()));
+                    },
                   ),
                 ],
               ),
