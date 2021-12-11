eb361ca502dbd082e0b57120696e5380881bff4a
MiyuSato <83227352+MiyuSato@users.noreply.github.com>
2021-12-11 12:47:49 +0900

    add file

---
+++ lib/qiita_qlient.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'article.dart';
+
+class QiitaClient {
+  static Future<List<Article>> fetchArticle() async {
+    final url = 'https://qiita.com/api/v2/items';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonArray = json.decode(response.body);
+      return jsonArray.map((json) => Article.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load article');
+    }
+  }
+}
